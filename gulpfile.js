d69aef6f0f4ed91344c61cb3d75f8160f50d3224
darya_kochubey <kochubeyalex71@gmail.com>
2018-08-11 18:23:01 +0200

    init commit

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+const gulp = require('gulp')
+const purgecss = require('gulp-purgecss')
+const sass = require('gulp-sass')
+const browserSync = require('browser-sync').create();
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+
+
+gulp.task('purgecss', ['sass'], () => {
+    return gulp
+      .src('project/css/*.css')
+      .pipe(
+        purgecss({
+          content: ['project/index.html']
+        })
+      )
+      .pipe(gulp.dest('project/css/'))
+    })
+
+gulp.task('postcss', ['sass', 'purgecss'], function () {
+    const plugins = [
+        autoprefixer({browsers: ['last 15 version']}),
+        cssnano()
+    ];
+    return gulp.src('project/css/*.css')
+        .pipe(postcss(plugins))
+        .pipe(gulp.dest('project/css'))
+        .pipe(browserSync.reload({
+            stream: true
+          }));
+});
+
+const pathToSass = 'project/sass/style.scss'
+
+gulp.task ('sass', function () {
+    return gulp.src(pathToSass)
+        .pipe(sass())
+        .pipe(gulp.dest('project/css'))
+});
+
+
+// watcher
+
+gulp.task('reload', function() {
+    browserSync.init({
+        server: {
+            baseDir: 'project'
+        },
+    })
+})
+
+gulp.task('watch', ['sass', 'purgecss', 'postcss', 'reload'], function() {
+    gulp.watch(pathToSass, ['sass', 'postcss']);
+    gulp.watch('project/index.html', browserSync.reload)
+});
+
+gulp.task('default', ['watch']);
